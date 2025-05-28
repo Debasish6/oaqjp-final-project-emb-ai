5dbe7047cba8ade6f9f06a8105844b89d96fe919
Debasish6 <iamdebasishghosh6@gmail.com>
2025-05-28 12:54:58 +0530

    Added new code which returns a dictionary

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,6 +14,31 @@ def emotion_detector(text_to_analyse):
     response = requests.post(URL, headers = Headers, json = data)
 
     if response.status_code == 200 :
-        return response.text
+        result = response.json()
+        emotions = result['emotionPredictions'][0]['emotion']
+
+        anger_score = emotions['anger']
+        disgust_score = emotions['disgust']
+        fear_score = emotions['fear']
+        joy_score = emotions['joy']
+        sadness_score = emotions['sadness']
+
+        emotion_scores = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+        return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+        }
     else:
         return {"error":response.status_code, "message":response.text}
